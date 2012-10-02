0e2cfb66a01912c2e6644d438481001ef0e65add
Marc Pomar Torres <boyander@gmail.com>
2012-10-02 02:05:55 +0200

    Lint & Comments

--- app.js
+++ app.js
@@ -1,51 +1,35 @@
 /**
  * Project Name: funny-clock
- * Author: Alguer Balanya, Marc Pomar, Victor Hidalgo
- * Contact: marc@faable.com
- * Description: Funny clock based on festival TTS with node.js frontend
+ * Author: Marc Pomar, Victor Hidalgo, Alguer Balanya
+ * Contact: marc@faable.com or boyander@gmail.com
+ * Description: Syntethized speech time based on festival TTS with node.js frontend
  */
 
 /*
  * Required modules
  */
-var express = require('express')
-  , http = require('http')
-  , fs = require('fs')
-  , sys = require('sys');
-
+var express = require('express'),
+  http = require('http'),
+  fs = require('fs'),
+  sys = require('sys'),
+  exec = require('child_process').exec;
 
 /*
- * Configuration (voice path)
+ * Configuration (setup path to custom festival voice)
  */
-
 // PRODUCTION
-//var voicePath = "/home/mpomar/time_ldom";
-
+// var voicePath = "/home/mpomar/time_ldom";
 // DEVELOPMENT
 var voicePath = "/Users/boyander/Desktop/tecno_parla/time_ldom";
-
-// Works starting on version 0.9 of node
-/*
-console.log("Check audio path...");
-try{
-  stats = fs.lstatSync("/Users/boyander/9bZkp7q19f0.aac");
-  if (!stats.isDirectory()) {
-    throw ENOENT;
-  }
-} catch(err) {
-  console.log("Voice dir not found, bye...");
-  process.exit(1);
-}
-*/
 
 /*
  * Express configuration
  */
-var app = express(); 
-app.configure(function(){
+var app = express();
+app.configure(function () {
   app.set('localrun', process.env.LOCAL || false);
   app.set('port', process.env.PORT || 8080);
-  app.set('title', "FunnyClock - Alguer, Marc, Victor")
+  app.set('title', "FunnyClock - Alguer, Marc, Victor");
   app.set('views', __dirname + '/templates');
   app.set('view engine', 'jade');
   app.set('view options', {
@@ -57,43 +41,41 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.errorHandler());
-  app.use(require('connect-assets')({'src':'assets'}));
+  app.use(require('connect-assets')({
+    'src': 'assets'
+  }));
   app.use(express.static(__dirname + '/public'));
 });
 
 /*
  * HTTP Server
  */
-server = http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
 });
-
-
 
 /*
  * Page routing
  */
- app.get('/', clock);
- app.get('/gettime',timeJSON);
+app.get('/', clock);
+app.get('/gettime', timeJSON);
 
 /*
  * Render main clock page
  */
-function clock(request, result){
-	var parameters = {
-		title: request.app.get("title"),
-		port: request.app.get("port")
-	}
-	result.render("index", parameters)
+function clock(request, result) {
+  var parameters = {
+    title: request.app.get("title"),
+    port: request.app.get("port")
+  };
+  result.render("index", parameters);
 }
 
 /*
- * JSON response
+ * JSON response for time request (user clicks on speaker)
  */
+function timeJSON(request, result) {
 
-var exec = require('child_process').exec;
-
-function timeJSON(request, result){
   // Current & last timestamps
   var currentTime = Math.round((new Date()).getTime() / 1000);
   var latestTime = request.app.get('latestTime') || 0;
@@ -101,51 +83,52 @@ function timeJSON(request, result){
   // Setup last file
   var currentWAV = "current" + latestTime + ".wav";
 
-  // Regenerate at least every minute
-  if (currentTime - latestTime > 60){
-    // Update current timestamp
+  // Regenerate WAV audio file if there's at least 1 minute from last generated
+  if (currentTime - latestTime > 60) {
+    // Update current timestamp & file first
     request.app.set('latestTime', currentTime);
+    currentWAV = "current" + currentTime + ".wav";
 
     // Setup output file path & voice paths
-    currentWAV = "current" + currentTime + ".wav";
     var savePath = __dirname + "/public/" + currentWAV;
 
     // Execute festival command to generate Waveform
     var command = "festival -b webservice_load.scm \"(savetime '" + savePath + ")\"";
     console.log("Regenerating Wav audio:\"" + command + "\"");
 
-    exec(command, {'cwd':voicePath}, function puts(error, stdout, stderr){
-      // View standard outputs on node log
+    // Executes festival asynchronously
+    exec(command, {
+      'cwd': voicePath
+    }, function puts(error, stdout, stderr) {
+      // View output from festival STDIO on node log
       sys.puts(stderr);
       sys.puts(stdout);
       console.log(error);
 
-      // Remove previous generated
-      if(latestTime != 0){
+      // Remove previous generated WAV file
+      if (latestTime !== 0) {
         var previousWAV = "public/current" + latestTime + ".wav";
         fs.unlink(previousWAV, function (err) {
-          if (err) console.log(err);
-          console.log('Deleted ' + previousWAV);
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('Deleted ' + previousWAV);
+          }
         });
       }
-      // Send object to client
+      // Send wav path to client
       result.contentType('application/json');
-      result.send({'wav':currentWAV});
+      result.send({
+        'wav': currentWAV
+      });
     });
 
-  }else{
+  } else {
+    // Send wav path as JSON to client
     console.log('Using pre syntetized time, regenerate on ' + Math.abs(currentTime - latestTime - 60) + ' seconds');
-    // Send object to client
     result.contentType('application/json');
-    result.send({'wav':currentWAV});
+    result.send({
+      'wav': currentWAV
+    });
   }
 }
-
-
-
-
-
-
-
-
-
--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,94 +1,91 @@
 var clock = {
-   
-  digits : ["zero", "one", "two", "three", "foure", "five", "six", "seven", "eight", "nine"],
-  
-  init : function(){
+
+  digits: ["zero", "one", "two", "three", "foure", "five", "six", "seven", "eight", "nine"],
+
+  init: function() {
     var $digit = $('.digit');
-    
+
     // Ugly....
-    this.hour = [$($digit[0]), $($digit[1])];    
-    this.min  = [$($digit[2]), $($digit[3])];    
-    this.sec  = [$($digit[4]), $($digit[5])];
-    
-    
-    this.drawInterval(this.drawSecond, function(time){
+    this.hour = [$($digit[0]), $($digit[1])];
+    this.min = [$($digit[2]), $($digit[3])];
+    this.sec = [$($digit[4]), $($digit[5])];
+
+
+    this.drawInterval(this.drawSecond, function(time) {
       return 1000 - time[3];
     })
-    
-    this.drawInterval(this.drawMinute, function(time){
+
+    this.drawInterval(this.drawMinute, function(time) {
       return 60000 - time[2] * 1000 - time[3];
     })
-    
-      
-   this.drawInterval(this.drawHour, function(time){
-     console.log((60 - time[1]) * 60000 - time[2] * 1000 - time[3])
+
+
+    this.drawInterval(this.drawHour, function(time) {
+      console.log((60 - time[1]) * 60000 - time[2] * 1000 - time[3])
       return (60 - time[1]) * 60000 - time[2] * 1000 - time[3];
-    })        
-   
+    })
+
   },
-  
-  getTimeArray : function(){
+
+  getTimeArray: function() {
     var dat = new Date();
     return [dat.getHours(), dat.getMinutes(), dat.getSeconds(), dat.getMilliseconds()];
   },
-    
-  drawInterval : function(func, timeCallback){
+
+  drawInterval: function(func, timeCallback) {
     var time = this.getTimeArray();
-    
-    func.call(this, time);    
-    
+
+    func.call(this, time);
+
     var that = this;
-    setTimeout(function(){
-      	that.drawInterval(func, timeCallback);      
+    setTimeout(function() {
+      that.drawInterval(func, timeCallback);
     }, timeCallback(time));
   },
-    
-  drawHour : function(time){
-  	this.drawDigits(this.hour, time[0]);  
+
+  drawHour: function(time) {
+    this.drawDigits(this.hour, time[0]);
   },
-  
-  drawMinute : function(time){
-  	this.drawDigits(this.min,  time[1]);  
+
+  drawMinute: function(time) {
+    this.drawDigits(this.min, time[1]);
   },
-  
-  drawSecond : function(time){  
-  	this.drawDigits(this.sec,  time[2]);
+
+  drawSecond: function(time) {
+    this.drawDigits(this.sec, time[2]);
   },
-  
-  drawDigits : function(digits, digit){
+
+  drawDigits: function(digits, digit) {
     var ten = Math.floor(digit / 10);
     var one = Math.floor(digit % 10);
-        
-    digits[0].attr('class', 'digit '+this.digits[ten]);
-    digits[1].attr('class', 'digit '+this.digits[one]);
+
+    digits[0].attr('class', 'digit ' + this.digits[ten]);
+    digits[1].attr('class', 'digit ' + this.digits[one]);
   }
-    
-}; 
-    
+
+};
+
 // Start Clock object
 clock.init();
 
 // Click on speaker
-$('#playtime').click(function(){
+$('#playtime').click(function () {
   console.log('Requesting audio object to server');
   playTime();
 });
 
-function playTime(){
+function playTime() {
   // Ajax request for current time
   $.ajax({
-      url: '/gettime',
-      dataType: 'json',
-      success: function(data) {
-        $.playSound(data.wav);
-      },
-      error: function(request,error) {
-        console.log("Cannot conect with audio server");
-      },
+    url: '/gettime',
+    dataType: 'json',
+    success: function (data) {
+      $.playSound(data.wav);
+    },
+    error: function (request, error) {
+      console.log("Cannot conect with audio server");
+    },
   });
 }
 
 playTime();
-
-
-
